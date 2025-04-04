5db049459310b4410c03f71626cc034329ce69b3
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-05 00:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+// Bootstrap form validation
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('intern-form');
+
+  form.addEventListener('submit', function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+  }, false);
+});
